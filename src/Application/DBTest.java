f04f26d98e3c234b49591b32360bb74fcb8947a7
proohit <46965017+proohit@users.noreply.github.com>
2019-03-31 20:06:50 +0200

    Update DBTest.java

--- src/Application/DBTest.java
+++ src/Application/DBTest.java
@@ -1,13 +1,11 @@
 package Application;
 
 import DBConnection.*;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 public class DBTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		DBManager db = new DBManager();
 		db.createDB("Accounting");
 		String[] cols = { "id","name"};
