76e6c39a4212bf58dd92a7510157c9030ed06c35
Direnc Timur <direnc99@gmail.com>
2019-08-08 20:17:17 +0200

    fixed alignment in operationsbox

--- src/gui/operations/OperationsBox.java
+++ src/gui/operations/OperationsBox.java
@@ -1,7 +1,9 @@
 package gui.operations;
 
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
@@ -9,10 +11,12 @@ public class OperationsBox extends VBox {
 	public OperationsBox() {
 		super();
 		Label operationsLabel = new Label("Operations");
-		operationsLabel.setAlignment(Pos.CENTER);
 		operationsLabel.setStyle("-fx-font-size: 14px;");
 		this.add(operationsLabel);
 		this.setStyle("-fx-padding: 10,0,0,0;");
+		
+		Separator separator1 = new Separator();
+		this.getChildren().add(separator1);
 		
 		Label recordOperations= new Label("Record operations");
 		recordOperations.setStyle("-fx-font-size: 14px");
@@ -22,11 +26,16 @@ public class OperationsBox extends VBox {
 		this.add(new OperationHyperlink("edit record..", "editRecord"));
 		this.add(new OperationHyperlink("delete record..", "deleteRecord"));
 		
+		Separator separator2 = new Separator();
+		this.getChildren().add(separator2);
+		
 		Label walletOperations= new Label("Wallet operations");
 		walletOperations.setStyle("-fx-font-size: 14px");
 		this.add(walletOperations);
 		
 		this.add(new OperationHyperlink("add wallet...", "addWallet"));
+		
+		this.setAlignment(Pos.TOP_CENTER);
 	}
 
 	public void add(Label label) {
