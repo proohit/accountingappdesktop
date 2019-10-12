f1ad3490b3f9974d457cca013281ff080617c37c
Direnc <direnc99@gmail.com>
2019-10-12 11:05:09 +0200

    alternative menubar update
    -implemented menubar. items pending

--- src/gui/database/DatabaseBox.java
+++ src/gui/database/DatabaseBox.java
@@ -7,6 +7,7 @@ import DBConnection.DBManager;
 import DBTables.RecordTable;
 import DBTables.WalletTable;
 import gui.Ui;
+import gui.operations.buttonHandler.SearchSettingsClickHandler;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.*;
@@ -15,18 +16,20 @@ import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class DatabaseBox extends HBox {
+public class DatabaseBox extends MenuBar {
 
-	public DatabaseBox() {
-		super();
+    public DatabaseBox() {
+        super();
 
-	}
+    }
 
-	public void initialize() {
-		Stage stage = (Stage) this.getScene().getWindow();
-		this.setStyle("-fx-padding: 10,0,0,10;");
-		Hyperlink newDatabase = new Hyperlink("new database...");
-		newDatabase.setOnAction(new EventHandler<ActionEvent>() {
+    public void initialize() {
+        Stage stage = (Stage) this.getScene().getWindow();
+        this.setStyle("-fx-padding: 10,0,0,10;");
+
+        Menu fileMenu = new Menu("File");
+        MenuItem newDatabase = new MenuItem("new database...");
+        newDatabase.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent arg0) {
@@ -53,15 +56,15 @@ public class DatabaseBox extends HBox {
 					createdDb.setOnHidden(new EventHandler<DialogEvent>() {
 						@Override
 						public void handle(DialogEvent event) {
-							if(Ui.configurator.isDefault(newFile.getAbsolutePath())) {
+							if (Ui.configurator.isDefault(newFile.getAbsolutePath())) {
 								ButtonType yes = new ButtonType("yes");
 								ButtonType no = new ButtonType("no");
 								Alert confirmation = new Alert(AlertType.NONE, "would you like to save this database as default? \n" +
 										"everytime you open the app, this database will be opened", yes, no);
 								confirmation.showAndWait().ifPresent(result -> {
-									if(result == yes) {
+									if (result == yes) {
 										Ui.configurator.writeEntry("defaultDb=" + newFile.getAbsolutePath());
-									} else if(result == no) {
+									} else if (result == no) {
 										confirmation.close();
 									}
 								});
@@ -71,8 +74,8 @@ public class DatabaseBox extends HBox {
 				}
 			}
 		});
-		Hyperlink openDatabase = new Hyperlink("open database...");
-		openDatabase.setOnAction(new EventHandler<ActionEvent>() {
+        MenuItem openDatabase = new MenuItem("open database...");
+        openDatabase.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent arg0) {
@@ -93,15 +96,15 @@ public class DatabaseBox extends HBox {
 					confirmation.setOnHidden(new EventHandler<DialogEvent>() {
 						@Override
 						public void handle(DialogEvent event) {
-							if(Ui.configurator.isDefault(existingFile.getAbsolutePath())) {
+							if (Ui.configurator.isDefault(existingFile.getAbsolutePath())) {
 								ButtonType yes = new ButtonType("yes");
 								ButtonType no = new ButtonType("no");
 								Alert confirmation = new Alert(AlertType.NONE, "would you like to save this database as default? \n" +
 										"everytime you open the app, this database will be opened", yes, no);
 								confirmation.showAndWait().ifPresent(result -> {
-									if(result == yes) {
+									if (result == yes) {
 										Ui.configurator.writeEntry("defaultDb=" + existingFile.getAbsolutePath());
-									} else if(result == no) {
+									} else if (result == no) {
 										confirmation.close();
 									}
 								});
@@ -111,8 +114,100 @@ public class DatabaseBox extends HBox {
 				}
 			}
 		});
+//        Hyperlink newDatabase = new Hyperlink("new database...");
+//        newDatabase.setOnAction(new EventHandler<ActionEvent>() {
+//
+//            @Override
+//            public void handle(ActionEvent arg0) {
+//                FileChooser fileChooser = new FileChooser();
+//                fileChooser.setTitle("create new Database");
+//                fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Database File", "*.db"));
+//                File newFile = fileChooser.showSaveDialog(stage);
+//                if (newFile != null) {
+//                    DBManager.createDB(newFile.getAbsolutePath());
+//                    try {
+//                        RecordTable.createTable();
+//                        WalletTable.createTable();
+//                    } catch (SQLException e) {
+//                        e.printStackTrace();
+//                    }
+//                    Ui.records.clear();
+//                    Ui.months.refreshAll();
+//                    Ui.wallets.refreshAll();
+//                    stage.setTitle("Accounting App - " + newFile.getName());
+//                    Alert createdDb = new Alert(AlertType.INFORMATION);
+//                    createdDb.setContentText("the database has been created at " + newFile.getAbsolutePath());
+//                    createdDb.show();
+//
+//                    createdDb.setOnHidden(new EventHandler<DialogEvent>() {
+//                        @Override
+//                        public void handle(DialogEvent event) {
+//                            if (Ui.configurator.isDefault(newFile.getAbsolutePath())) {
+//                                ButtonType yes = new ButtonType("yes");
+//                                ButtonType no = new ButtonType("no");
+//                                Alert confirmation = new Alert(AlertType.NONE, "would you like to save this database as default? \n" +
+//                                        "everytime you open the app, this database will be opened", yes, no);
+//                                confirmation.showAndWait().ifPresent(result -> {
+//                                    if (result == yes) {
+//                                        Ui.configurator.writeEntry("defaultDb=" + newFile.getAbsolutePath());
+//                                    } else if (result == no) {
+//                                        confirmation.close();
+//                                    }
+//                                });
+//                            }
+//                        }
+//                    });
+//                }
+//            }
+//        });
+//        Hyperlink openDatabase = new Hyperlink("open database...");
+//        openDatabase.setOnAction(new EventHandler<ActionEvent>() {
+//
+//            @Override
+//            public void handle(ActionEvent arg0) {
+//                FileChooser fileChooser = new FileChooser();
+//                fileChooser.setTitle("open existing database");
+//                fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Database File", "*.db"));
+//                File existingFile = fileChooser.showOpenDialog(stage);
+//                if (existingFile != null) {
+//                    DBManager.createDB(existingFile.getAbsolutePath());
+//                    Ui.months.refreshAll();
+//                    Ui.wallets.refreshAll();
+//                    Ui.records.clear();
+//                    stage.setTitle("Accounting App - " + existingFile.getName());
+//                    Alert confirmation = new Alert(AlertType.INFORMATION);
+//                    confirmation.setContentText("the database has been loaded from " + existingFile.getAbsolutePath());
+//                    confirmation.show();
+//
+//                    confirmation.setOnHidden(new EventHandler<DialogEvent>() {
+//                        @Override
+//                        public void handle(DialogEvent event) {
+//                            if (Ui.configurator.isDefault(existingFile.getAbsolutePath())) {
+//                                ButtonType yes = new ButtonType("yes");
+//                                ButtonType no = new ButtonType("no");
+//                                Alert confirmation = new Alert(AlertType.NONE, "would you like to save this database as default? \n" +
+//                                        "everytime you open the app, this database will be opened", yes, no);
+//                                confirmation.showAndWait().ifPresent(result -> {
+//                                    if (result == yes) {
+//                                        Ui.configurator.writeEntry("defaultDb=" + existingFile.getAbsolutePath());
+//                                    } else if (result == no) {
+//                                        confirmation.close();
+//                                    }
+//                                });
+//                            }
+//                        }
+//                    });
+//                }
+//            }
+//        });
+        Hyperlink settings = new Hyperlink("settings...");
+        settings.setOnAction(new SearchSettingsClickHandler());
 
-		this.getChildren().add(newDatabase);
-		this.getChildren().add(openDatabase);
-	}
+        fileMenu.getItems().addAll(newDatabase, openDatabase);
+        this.getMenus().add(fileMenu);
+
+//        this.getChildren().add(newDatabase);
+//        this.getChildren().add(openDatabase);
+        this.getChildren().add(settings);
+    }
 }
